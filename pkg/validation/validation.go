148ade690cb9c605c0fec01ee700073b59143f75
Diego Parra <diegoparraferreira@gmail.com>
2022-05-25 16:09:05 -0400

    feat: add validation package

---
+++ pkg/validation/validation.go
@@ -0,0 +1,13 @@
+package validation
+
+func Validate(d, c []string) bool {
+	if len(d) != len(c) {
+		return false
+	}
+	for i, v := range d {
+		if v != c[i] {
+			return false
+		}
+	}
+	return true
+}
