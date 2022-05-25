accf49f2edad7a6175fe5ff2014c7838b533b46c
Diego Parra <dp@linkfire.com>
2022-05-24 21:39:40 -0400

    feat: add function getAwsNS which give us the desired NS

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net"
+	"sort"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -20,27 +20,84 @@ func main() {
 
 	// fmt.Println(domains)
 	for _, v := range domains {
+		fmt.Println("")
 		fmt.Println("Domain: ", v)
+		fmt.Println("")
+		fmt.Println("Current NS: ")
+
 		getNS(v)
+
 		id, err := getHostedZoneID(v)
 		if err != nil {
 			fmt.Println(err)
 		}
+
+		fmt.Println("")
 		fmt.Println("HostedZoneID: ", strings.TrimPrefix(*id.Id, "/hostedzone/"))
+
+		fmt.Println("")
+		fmt.Println("Desired NS: ")
+		getAwsNS(strings.TrimPrefix(*id.Id, "/hostedzone/"), v)
+
+		fmt.Println("")
 		fmt.Println("End domain")
 	}
 }
 
-func getNS(d string) {
+func getAwsNS(zID string, zNAME string) {
 
-	n, _ := net.LookupNS(d)
-
-	out, err := json.Marshal(n)
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1"),
+	})
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("failed to create session,", err)
+		// return nil, err
 	}
 
-	fmt.Println(string(out))
+	svc := route53.New(sess)
+
+	resp, err := svc.ListResourceRecordSets(&route53.ListResourceRecordSetsInput{
+		HostedZoneId:    aws.String(zID),
+		StartRecordName: aws.String(zNAME),
+		StartRecordType: aws.String("NS"),
+	})
+	if err != nil {
+		fmt.Println("Caiu no error do resp")
+		fmt.Println(err)
+		// return nil, err
+	}
+	if len(resp.ResourceRecordSets) == 0 {
+		fmt.Println("Nothing found")
+		// return nil, nil
+	}
+	ns := make([]string, len(resp.ResourceRecordSets[0].ResourceRecords))
+	for i := range resp.ResourceRecordSets[0].ResourceRecords {
+		ns[i] = *resp.ResourceRecordSets[0].ResourceRecords[i].Value
+	}
+	sort.Strings(ns)
+	fmt.Println(ns)
+}
+
+func getNS(d string) {
+	var nss []string
+	n, _ := net.LookupNS(d)
+
+	for _, v := range n {
+		nss = append(nss, v.Host)
+	}
+
+	// out, err := json.Marshal(n)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// for k, v := range out {
+	// 	fmt.Println(k)
+	// 	fmt.Println(v)
+	// 	nss = append(nss)
+	// }
+
+	fmt.Println(nss)
 }
 
 func getHostedZoneID(d string) (*route53.HostedZone, error) {
