f14b206290bdafe83564f466a23a7d95f356c027
Diego Parra <diegoparraferreira@gmail.com>
2022-05-26 13:23:26 -0400

    fix: print only domains with error

--- cmd/ns-checker/main.go
+++ cmd/ns-checker/main.go
@@ -16,38 +16,41 @@ func main() {
 		fmt.Println(err)
 	}
 
+	// Go over Domain list
 	for _, v := range d {
-		// Go over Domain list
-		fmt.Println("")
-		fmt.Println("Domain: ", v)
+		// fmt.Println("")
+		// fmt.Println("Domain: ", v)
 
 		//Get HostedZoneID
 		id, err := r53.GetHostedZoneID(v)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("HostedZoneID: ", strings.TrimPrefix(*id.Id, "/hostedzone/"))
+		// fmt.Println("HostedZoneID: ", strings.TrimPrefix(*id.Id, "/hostedzone/"))
 
 		// Get Current NS
 		ns, err := r53.GetNS(v)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("Current NS: ", ns)
+		// fmt.Println("Current NS: ", ns)
 
 		// Get Desired NS
 		desiredNS, err := r53.GetRecordType(strings.TrimPrefix(*id.Id, "/hostedzone/"), v, "NS")
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("Desired NS: ", desiredNS)
+		// fmt.Println("Desired NS: ", desiredNS)
 
 		// Validate Desired NS with Current NS
 		val := utils.Validate(desiredNS, ns)
 		if val != true {
-			fmt.Println("Domain: " + v + " not poiting to Linkfire NS's")
-		} else {
-			fmt.Println("Domain: " + v + " NS's working fine")
+			fmt.Println("")
+			fmt.Println("Status: Error validating NS")
+			fmt.Println("Domain: ", v)
+			fmt.Println("Desired NS: ", desiredNS)
+			fmt.Println("Current NS: ", ns)
+			fmt.Println("")
 		}
 
 		// TODO
