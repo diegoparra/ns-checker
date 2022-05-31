d3abedcf5a3cc4ba95af688f1786ab8127cbf3aa
Diego Parra <dp@linkfire.com>
2022-05-30 22:53:58 -0400

    fix: added new regex

--- pkg/acm/acm.go
+++ pkg/acm/acm.go
@@ -38,7 +38,9 @@ func ListCertificate() ([]string, error) {
 	}
 
 	for _, v := range resp.CertificateSummaryList {
-		found, err := regexp.MatchString(`(?:linkfire.co|metafire.co|linkfire.com|lnk.to|lnktest.dk|mit.mpo.dk|server|lnk.tt|amazonaws.com)`, string(*v.DomainName))
+		found, err := regexp.MatchString(`(?:linkfire|metafire|lnktest|lnk|mpo|linkfire-partners|smarturl|bio|server|amazonaws|sonymusic|lexijayde)\.(?:co|com|dk|it|tt|to|link)`, string(*v.DomainName))
+		// server|amazonaws.com|linkfire-partners.com|smarturl.it|linkfire.tobis.de|*.bio.to|lexijayde.link|ads.sonymusic.com.br)`, string(*v.DomainName))
+		// found, err := regexp.MatchString(`(?:([-a-z0-9]+)\.linkfire\.co|metafire.co|linkfire.com|lnk.to|lnktest.dk|mit.mpo.dk|server|lnk.tt|amazonaws.com|linkfire-partners.com|smarturl.it|linkfire.tobis.de|*.bio.to|lexijayde.link|ads.sonymusic.com.br)`, string(*v.DomainName))
 		if err != nil {
 			fmt.Println("Error to run regex match string", err)
 		}
